c5250daac2554c764e200e9ce8e7e5d313f1a1e5
Terry Johnson <giovanni7304@me.com>
2016-10-31 21:56:47 -0500

    Initial Commit

---
+++ Pod Player/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pod Player
+//
+//  Created by Terry Johnson on 10/31/16.
+//  Copyright © 2016 Terry Johnson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
