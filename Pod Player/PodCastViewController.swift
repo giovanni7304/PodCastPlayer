0dbcec53af0200bc20f6458f2cfec81314153622
Terry Johnson <giovanni7304@me.com>
2016-10-31 22:21:18 -0500

    Initial Commit

---
+++ Pod Player/PodCastViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PodCastViewController.swift
+//  Pod Player
+//
+//  Created by Terry Johnson on 10/31/16.
+//  Copyright © 2016 Terry Johnson. All rights reserved.
+//
+
+import Cocoa
+
+class PodCastViewController: NSViewController {
+
+    @IBOutlet weak var podcastsURLTextField: NSTextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+    @IBAction func addPodcastsClicked(_ sender: AnyObject) {
+    }
+}
